47a23d6fc6161d9649aec84d9706f0c5ecd1443c
Gycks <115739329+Gycks@users.noreply.github.com>
2026-02-19 13:51:54 +0100

    version check against tag on release (#3)

--- src/transctl/cli/commands/run.py
+++ src/transctl/cli/commands/run.py
@@ -4,8 +4,13 @@ import click
 
 
 @click.command('run', help='Run the translation process.')
+@click.option("-g", "--glossary", help="Path to glossary file (JSON).", default="")
 @click.pass_context
-def run(ctx: click.Context) -> None:
+def run(ctx: click.Context, glossary: str) -> None:
     if ctx.invoked_subcommand is None:
         coordinator: TranslationCoordinator = TranslationCoordinator()
-        coordinator.translate_from_config()
+
+        if glossary:
+            coordinator.translate_from_config(glossary)
+        else:
+            coordinator.translate_from_config()
